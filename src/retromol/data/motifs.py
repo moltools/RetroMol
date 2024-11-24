a0d9400fdcca03bdaf2c9298728f4e5880b20d00
David Meijer <david-meijer@live.nl>
2024-11-24 16:28:47 +0100

    WIP: adding motif matching rules

--- src/retromol/data/motifs.py
+++ src/retromol/data/motifs.py
@@ -1,1 +1,90 @@
-MOTIFS = []
+MOTIFS = [
+    {
+        "name": "A1",
+        "smiles": "OSC(=O)CC(=O)O",
+    },
+    {
+        "name": "A2",
+        "smiles": "OSC(=O)C(C)C(=O)O",
+    },
+    {
+        "name": "A3",
+        "smiles": "OSC(=O)C(C)(C)C(=O)O",
+    },
+    {
+        "name": "A4",
+        "smiles": "OSC(=O)C(CC)C(=O)O",
+    },
+    {
+        "name": "A5",
+        "smiles": "OSC(=O)C(O)C(=O)O",
+    },
+    {
+        "name": "A6",
+        "smiles": "OSC(=O)C(C)(O)C(=O)O",
+    },
+    {
+        "name": "B1",
+        "smiles": "OSC(O)CC(=O)O",
+    },
+    {
+        "name": "B2",
+        "smiles": "OSC(O)C(C)C(=O)O",
+    },
+    {
+        "name": "B3",
+        "smiles": "OSC(O)C(C)(C)C(=O)O",
+    },
+    {
+        "name": "B4",
+        "smiles": "OSC(O)C(CC)C(=O)O",
+    },
+    {
+        "name": "B5",
+        "smiles": "OSC(O)C(O)C(=O)O",
+    },
+    {
+        "name": "B6",
+        "smiles": "OSC(O)C(C)(O)C(=O)O",
+    },
+    {
+        "name": "C1",
+        "smiles": "OSC=CC(=O)O",
+    },
+    {
+        "name": "C2",
+        "smiles": "OSC=C(C)C(=O)O",
+    },
+    {
+        "name": "C5",
+        "smiles": "OSC=C(O)C(=O)O",
+    },
+    {
+        "name": "D1",
+        "smiles": "OSC-CC(=O)O",
+    },
+    {
+        "name": "D2",
+        "smiles": "OSC-C(C)C(=O)O",
+    },
+    {
+        "name": "D3",
+        "smiles": "OSC-C(C)(C)C(=O)O",
+    },
+    {
+        "name": "D4",
+        "smiles": "OSC-C(CC)C(=O)O",
+    },
+    {
+        "name": "D5",
+        "smiles": "OSC-C(O)C(=O)O",
+    },
+    {
+        "name": "D6",
+        "smiles": "OSC-C(C)(O)C(=O)O",
+    },
+    {
+        "name": "glycine",
+        "smiles": "C(C(=O)O)N",
+    }
+]
