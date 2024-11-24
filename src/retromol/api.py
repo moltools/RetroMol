333fa44f60b1ec787db1b48673ad0a31e760049e
David Meijer <david-meijer@live.nl>
2024-11-24 02:43:19 +0100

    ADD: mock function for running RetroMol on SMILES

--- src/retromol/api.py
+++ src/retromol/api.py
@@ -1,1 +1,8 @@
-# TODO
+from time import sleep
+from random import randint
+
+
+def run_retromol(smiles: str) -> None:
+    """Parse a SMILES string."""
+    # sleep random number of seconds between 1 and 7 to simulate different run times
+    sleep(randint(1, 7))
