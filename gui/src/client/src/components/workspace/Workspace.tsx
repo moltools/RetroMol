f1b8fd964c7bd3bf208ee09b21c94f8e9c931596
David Meijer <david-meijer@live.nl>
2026-05-28 13:35:09 -0400

    WIP: updating discovery tab

--- gui/src/client/src/components/workspace/Workspace.tsx
+++ gui/src/client/src/components/workspace/Workspace.tsx
@@ -13,7 +13,6 @@ import { WorkspaceSideMenu } from "./WorkspaceSideMenu";
 import { WorkspaceHeader } from "./WorkspaceHeader";
 import { WorkspaceHome } from "./WorkspaceHome";
 import { WorkspaceUpload } from "./WorkspaceUpload";
-import { WorkspaceDiscovery } from "./WorkspaceDiscovery";
 // import { WorkspaceEnrichment } from "./tabs/enrichment/WorkspaceEnrichment";
 
 export const Workspace: React.FC = () => {
@@ -131,9 +130,10 @@ export const Workspace: React.FC = () => {
                 <Routes>
                   <Route index element={<WorkspaceHome />} />
                   <Route path="upload" element={<WorkspaceUpload session={session} setSession={setSession} />} />
-                  <Route path="discovery" element={<WorkspaceDiscovery session={session} setSession={setSession} />} />
+                  {/*<Route path="discovery" element={<WorkspaceDiscovery session={session} setSession={setSession} />} />*/}
+                  <Route path="discovery" element={<div>Analysis currently available. Check back later.</div>} />
                   {/*<Route path="enrichment" element={<WorkspaceEnrichment session={session} setSession={setSession} />} />*/}
-                    <Route path="enrichment" element={<div>Analysis currently available. Check back later.</div>} />
+                  <Route path="enrichment" element={<div>Analysis currently available. Check back later.</div>} />
                 </Routes>
               </Box>
             </Fade>
