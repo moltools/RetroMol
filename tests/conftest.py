b2606e2171d2eaa9f78108368e3b3850c6614750
David Meijer <david-meijer@live.nl>
2026-08-19 15:13:06 -0400

    ENH: E/Z stereochemistry is also registered after being introduced by rule mid-parsing

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 
 from retromol.model.rules import RuleSet
 
-from .helpers_retromol import load_rule_set
+from .helpers_retromol import load_rule_set, load_stereo_rule_set
 
 
 @pytest.fixture(scope="session")
@@ -15,3 +15,13 @@ def ruleset() -> RuleSet:
     :return: the loaded RuleSet object
     """
     return load_rule_set()
+
+
+@pytest.fixture(scope="session")
+def stereo_ruleset() -> RuleSet:
+    """
+    Load rule set once per test session, matching stereochemistry.
+
+    :return: the loaded RuleSet object
+    """
+    return load_stereo_rule_set()
